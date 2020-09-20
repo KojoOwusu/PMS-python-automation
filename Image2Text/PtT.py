94515682c8797c894918e2d1e165056426033523
Kwadwo Owusu <35710156+KojoOwusu@users.noreply.github.com>
2020-09-20 19:55:35 +0000

    Add files via upload

---
+++ Image2Text/PtT.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import pytesseract
+import os
+
+PATH_TO_IMG = "/img"
+print(os.getcwd())
+os.
+
+Image.open()
+pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
+
